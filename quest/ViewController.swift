1b422042399091e100424fcc6653fdb134984bd6
Nathan Tornquist <Tornquist@live.com>
2017-05-08 07:56:16 -0500

    Initial Commit

---
+++ quest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quest
+//
+//  Created by Nathan Tornquist on 5/8/17.
+//  Copyright © 2017 nathantornquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
