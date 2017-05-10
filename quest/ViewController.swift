afe2766d5b424ac340582e439996970c9deb22a9
Nathan Tornquist <Tornquist@live.com>
2017-05-09 21:44:28 -0500

    Add rough compass pointing north

--- quest/ViewController.swift
+++ quest/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var angleLabel: UILabel!
+    @IBOutlet weak var accuracyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.requestLocationAccess()
     }
 
-
+    func requestLocationAccess() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        // 3
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.requestLocation()
+        }
+        
+        if CLLocationManager.headingAvailable() {
+            locationManager.headingFilter = 5
+            locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        guard newHeading.headingAccuracy > 0 else {
+            return
+        }
+        self.accuracyLabel.text = "Accuracy: \(newHeading.headingAccuracy)"
+        
+        let degrees = newHeading.trueHeading > 0 ? newHeading.trueHeading : newHeading.magneticHeading
+        let radians = degrees * Double.pi / 180
+        
+        self.angleLabel.text = "Degrees: \(degrees)\nRadians: \(radians)"
+        self.rotateLineTo(-radians)
+    }
+    
+    func rotateLineTo(_ radians: Double) {
+        lineView.transform = CGAffineTransform.identity
+        lineView.transform = CGAffineTransform(rotationAngle: CGFloat(radians))
+    }
 }
 
