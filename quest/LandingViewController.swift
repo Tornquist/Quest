0a98d08d0e59777f04d845334d4ddef4c8823446
Nathan Tornquist <Tornquist@live.com>
2017-05-25 07:41:17 -0500

    Working permissions lifecycle on landing screen

--- quest/LandingViewController.swift
+++ quest/LandingViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 import CoreLocation
 import AVFoundation
 
-class LandingViewController: UIViewController {
+class LandingViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     var needsLocation = true
     var deniedLocation = false
@@ -22,13 +25,14 @@ class LandingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let locationStatus = CLLocationManager.authorizationStatus()
-        self.needsLocation = locationStatus != CLAuthorizationStatus.authorizedWhenInUse
-        self.deniedLocation = locationStatus == CLAuthorizationStatus.denied
+        locationManager.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        let cameraStatus = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
-        self.needsCamera = cameraStatus != AVAuthorizationStatus.authorized
-        self.deniedCamera = cameraStatus == AVAuthorizationStatus.denied
+        self.refreshAuth()
+        self.updateText()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -40,12 +44,70 @@ class LandingViewController: UIViewController {
     }
     
     @IBAction func continueButtonPressed(_ sender: UIButton) {
-        self.showHomescreen(animated: true)
+        self.authPrompt()
     }
     
     func showHomescreen(animated: Bool) {
         let mainVC = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
         self.present(mainVC, animated: animated, completion: nil)
+    }
+    
+    // MARK: - Copy Changes
+    
+    func updateText() {
+        if self.showSettings() {
+            self.descriptionLabel.text = "NavQuest is a GPS and camera based scavenger hunt. It requires camera and location access to function. To play the game, please press settings to open the settings menu and grant access"
+            self.continueButton.setTitle("Settings", for: .normal)
+        } else {
+            self.descriptionLabel.text = "NavQuest is a GPS and camera based scavenger hunt. It requires camera and location access to function. To play the game, please press continue and then grant access."
+            self.continueButton.setTitle("Continue", for: .normal)
+        }
+    }
+    
+    // MARK: - Permissions Lifecycle
+    
+    func showSettings() -> Bool {
+        return self.deniedCamera || self.deniedLocation
+    }
+    
+    func refreshAuth() {
+        let locationStatus = CLLocationManager.authorizationStatus()
+        self.needsLocation = locationStatus != CLAuthorizationStatus.authorizedWhenInUse
+        self.deniedLocation = locationStatus == CLAuthorizationStatus.denied
+        
+        let cameraStatus = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        self.needsCamera = cameraStatus != AVAuthorizationStatus.authorized
+        self.deniedCamera = cameraStatus == AVAuthorizationStatus.denied
+    }
+    
+    func authPrompt() {
+        if self.showSettings() {
+            UIApplication.shared.open(URL(string:UIApplicationOpenSettingsURLString)!)
+        } else if self.needsLocation {
+            locationManager.requestWhenInUseAuthorization()
+        } else if self.needsCamera {
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo) { granted in
+                DispatchQueue.main.async {
+                    self.authHandler(success: granted)
+                }
+            }
+        } else {
+            self.showHomescreen(animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            self.authHandler(success: status == .authorizedWhenInUse)
+        }
+    }
+    
+    func authHandler(success: Bool) {
+        self.refreshAuth()
+        self.updateText()
+        if success {
+            self.authPrompt()
+        }
     }
 }
 
